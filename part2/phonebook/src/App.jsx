0e1174a0a5f504b6d9d546ac4466b1ea3ae367fc
AntonyShaps <16188347+AntonyShaps@users.noreply.github.com>
2023-12-25 15:11:01 +0100

    Implement back-end functionality 2.12-2.15

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,22 +1,20 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-
+import personService from './services/persons'
 import Filter from './components/Filter'
 import Numbers from './components/Numbers'
 import PeopleAdder from './components/PeopleAdder'
 
 const App = (props) => {
   const [persons, setPersons] = useState([])
-  useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
-      })
-  }, [])
-  console.log('render', persons.length, 'notes')
+  useEffect(() =>{
+    personService
+    .getAll()
+    .then(initialPersons => {
+      setPersons(initialPersons)
+    })
+  },[])
+  console.log('render', persons.length, 'persons')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setNewFilterName] = useState('')
@@ -27,20 +25,41 @@ const App = (props) => {
       name: newName,
       number: newNumber
     }
-    const nameExists = persons.some(person => person.name === newName)
+    const nameExists = persons.find(person => person.name === newName)
     if (nameExists) {
-      alert(`${newName} is already added to phonebook`)
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one`))
+      {
+        personService
+        .update(nameExists.id, nameObject)
+        .then(returnedName => {
+          setPersons(persons.map(person => person.id !== nameExists.id ? person : returnedName))
+          setNewName('')
+          setNewNumber('')
+        })
+      }
     } else {
-      setPersons(persons.concat(nameObject))
-      setNewName('')
-      setNewNumber('')
+      personService
+      .create(nameObject)
+      .then(returnedName => {
+        setPersons(persons.concat(returnedName))
+        setNewName('')
+        setNewNumber('')
+      })
     }
   }
 
   const handleNameChange = (event) => {setNewName(event.target.value)}
   const handleNumberChange = (event) => {setNewNumber(event.target.value)}
   const handleFilterName = (event) => {setNewFilterName(event.target.value.toLowerCase())}
-
+  const removeName = id =>{
+    const person = persons.find(n => n.id === id)
+    if (window.confirm(`Delete ${person.name}?`)) {
+      personService
+        .remove(id)
+        .then(() => {
+          setPersons(persons.filter(p => p.id !== id))
+        })    
+  }}
   return (
     <div>
       <h2>Phonebook</h2>
@@ -49,7 +68,7 @@ const App = (props) => {
         <PeopleAdder addName={addName} newName={newName} handleNameChange={handleNameChange}
       newNumber={newNumber} handleNumberChange={handleNumberChange}/>
       <h2>Numbers</h2>
-        <Numbers persons={persons} filterName={filterName}/>
+        <Numbers persons={persons} filterName={filterName} removeName={removeName}/>
     </div>
   )
 }
--- part2/phonebook/src/components/Numbers.jsx
+++ part2/phonebook/src/components/Numbers.jsx
@@ -1,13 +1,16 @@
 
 
 
-const Numbers = ({ persons, filterName }) => {
+const Numbers = ({ persons, filterName, removeName }) => {
     return (
       <div>
         {persons
           .filter(person => person.name.toLowerCase().includes(filterName.toLowerCase()))
           .map(person => (
-            <div key={person.name}>{person.name} {person.number}</div>
+            <div key={person.name}>
+              {person.name} {person.number}
+              <button onClick={()=>removeName(person.id)}>delete</button>
+            </div>
           ))
         }
       </div>
