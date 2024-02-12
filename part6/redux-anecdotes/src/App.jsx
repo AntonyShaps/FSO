8f230de57bea2774998cdec921b9e078f5d6a4e9
AntonyShaps <16188347+AntonyShaps@users.noreply.github.com>
2024-02-12 02:07:22 +0100

    6.14-6.15

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -2,8 +2,17 @@ import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import { useEffect } from 'react'
+import anecdoteServive from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
+
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    anecdoteServive.getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+  },)
   return (
     <div>
       <h2>Anecdotes</h2>
--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,18 +1,21 @@
 import { useDispatch } from 'react-redux'
-import { showNotification, hideNotification } from './../reducers/notificationReducer';
+import { showNotification, hideNotification } from './../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 
 import { createAnecdote } from './../reducers/anecdoteReducer'
 const AnecdoteForm = () => {
     const dispatch = useDispatch()
 
-    const addAnecdote = (event) => {
+    const addAnecdote = async (event) => {
         event.preventDefault()
         console.log(event.target)
         const content = event.target.anecdotes.value
         event.target.anecdotes.value = ''
-        dispatch(createAnecdote(content))
-        dispatch(showNotification(`Anecdote '${content}' added!`))
+        const newAnecdote = await anecdoteService.createNew(content)
+        console.log(typeof newAnecdote.content)
+        dispatch(createAnecdote(newAnecdote))
+        dispatch(showNotification(`Anecdote '${newAnecdote.content}' added!`))
         setTimeout(() => {
             dispatch(hideNotification())
         }, 5000)
